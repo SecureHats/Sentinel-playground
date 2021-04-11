2500204f18e43d518831fd8b2335d95b2b4dd26e
SecureHats <72928684+SecureHats@users.noreply.github.com>
2021-04-11 21:42:09 +0200

    reverted some changes

--- PowerShell/Add-AzureMonitorLogs/Add-AzureMonitorLogs.ps1
+++ PowerShell/Add-AzureMonitorLogs/Add-AzureMonitorLogs.ps1
@@ -90,118 +90,123 @@ Function Set-LogAnalyticsData {
     else {
         Write-Output "Uploaded to Data Log Collector table [$($logType + '_CL')] at [$rfc1123date]"
     }
+}
 
-    try {
-        Write-Output "Looking for requested workspace [$($WorkspaceName)]"
-        $workspaceParams = @{
-            Method               = 'GET'
-            ApiVersion           = '2020-08-01'
-            ResourceProviderName = 'Microsoft.OperationalInsights/workspaces'
-        }
-
-        $workspace = ((Invoke-AzRestMethod @workspaceParams).Content `
-            | ConvertFrom-Json).value `
-            | Where-Object Name -eq $WorkspaceName
-
-        $splitArray         = $workspace.id -split '/'
-        $ResourceGroupName  = $splitArray[4]
-        $WorkspaceName      = $splitArray[8]
-        $workspaceId        = $workspace.properties.customerId
-
-    }
-    catch {
-        Write-Warning -Message "Log Analytics workspace [$($WorkspaceName)] not found in the current context"
-        break
+try {
+    Write-Output "Looking for requested workspace [$($WorkspaceName)]"
+    $workspaceParams = @{
+        Method               = 'GET'
+        ApiVersion           = '2020-08-01'
+        ResourceProviderName = 'Microsoft.OperationalInsights/workspaces'
     }
 
-    $workspaceKey = (Get-AzOperationalInsightsWorkspaceSharedKeys `
-        -ResourceGroupName $ResourceGroupName `
-        -Name $WorkspaceName).PrimarySharedKey `
-        | ConvertTo-SecureString -AsPlainText -Force
+    $workspace = ((Invoke-AzRestMethod @workspaceParams).Content `
+        | ConvertFrom-Json).value `
+        | Where-Object Name -eq $WorkspaceName
 
-    if ($PSCmdlet.ParameterSetName -eq "CloudRepo") {
-            $uriArray = $repoUri.Split("/")
-            $gitOwner = $uriArray[3]
-            $gitRepo = $uriArray[4]
-            | Where-Object { $_.Name -like "*$($uriArray[8])*" -and $_.type -eq 'dir' }
+    $splitArray         = $workspace.id -split '/'
+    $ResourceGroupName  = $splitArray[4]
+    $WorkspaceName      = $splitArray[8]
+    $workspaceId        = $workspace.properties.customerId
+}
+catch {
+    Write-Warning -Message "Log Analytics workspace [$($WorkspaceName)] not found in the current context"
+    break
+}
 
-        $folders = $response `
+$workspaceKey = (Get-AzOperationalInsightsWorkspaceSharedKeys `
+    -ResourceGroupName $ResourceGroupName `
+    -Name $WorkspaceName).PrimarySharedKey `
+    | ConvertTo-SecureString -AsPlainText -Force
+
+if ($PSCmdlet.ParameterSetName -eq "CloudRepo") {
+    $uriArray = $repoUri.Split("/")
+    $gitOwner = $uriArray[3]
+    $gitRepo = $uriArray[4]
+    $gitPath = $uriArray[7]
+    
+    $apiUri = "https://api.github.com/repos/$gitOwner/$gitRepo/contents/$gitPath"
+
+    $response = (Invoke-WebRequest $apiUri).Content `
+        | ConvertFrom-Json `
+        | Where-Object { $_.Name -like "*$($uriArray[8])*" -and $_.type -eq 'dir' } `
+        | Select-Object Name
+
+    $folders = $response `
         | Where-Object { $_.Name -notlike "*.*" } `
         | Select-Object Name
 
-        foreach ($subfolder in $folders.Name) {
-            $apiUri = "https://api.github.com/repos/$gitOwner/$gitRepo/contents/$gitPath/$subfolder"
-            Write-Host "New URL: [$apiUri]"
+    foreach ($subfolder in $folders.Name) {
+        $apiUri = "https://api.github.com/repos/$gitOwner/$gitRepo/contents/$gitPath/$subfolder"
+        
+        $webResponse = (Invoke-WebRequest $apiUri).Content | ConvertFrom-Json
+        $dataUris = `
+            ($webResponse `
+                | Where-Object { `
+                    $_.download_url -like "*.json" `
+                    <#-or `
+                    $_.download_url -like "*.csv" #>} `
+            ).download_url
 
-            $webResponse = (Invoke-WebRequest $apiUri).Content | ConvertFrom-Json
-            $dataUris = `
-                ($webResponse `
-                    | Where-Object { `
-                        $_.download_url -like "*.json" -or `
-                        $_.download_url -like "*.csv" } `
-                ).download_url
-
-            foreach ($uri in $dataUris) {
-                write-output "uri: [$($uri)]"
-
-                $dataFile = Invoke-RestMethod -Method Get -Uri $uri | ConvertTo-Json
-                $namedTable = ($uri -split '/')[-1]
-                if ($namedTable -like "*_CL*") {
-                    #temporarly store initial custom table name
-                    $_customTableName = $CustomTableName
-                    $CustomTableName = ($namedTable -replace "_CL.json")
-                }
-
-                Set-LogAnalyticsData `
-                    -WorkspaceId $workspaceId `
-                    -WorkspaceKey $workspaceKey `
-                    -body ([System.Text.Encoding]::UTF8.GetBytes($dataFile)) `
-                    -logType $CustomTableName
-
-                #reset to initial value
-                if ($_customTableName) {
-                    $CustomTableName = $_customTableName
-                }
-            }
-        }
-    }
-    elseif ($PSCmdlet.ParameterSetName -eq "LocalRepo") {
-        $dataFiles = @(Get-ChildItem `
-            -Path $repoDirectory `
-            -File `
-            -Recurse `
-            -Include "*.json", "*.csv")
-
-        foreach ($dataFile in $dataFiles) {
-            Write-Output "Retrieving content from data file [$dataFile]"
-            switch ($dataFile.extension) {
-                ".json" {
-                    Write-Output "Processing [$dataFile]"
-                    $content = Get-Content -Path $dataFile.FullName -Raw
-                }
-                ".csv" {
-                    Write-Output "Processing [$dataFile]"
-                    $content = Get-Content -Path $dataFile.FullName -Raw `
-                    | ConvertFrom-Csv `
-                    | ConvertTo-Json
-                }
+        foreach ($uri in $dataUris) {
+            $dataFile = Invoke-RestMethod -Method Get -Uri $uri | ConvertTo-Json
+            $namedTable = ($uri -split '/')[-1]
+            
+            if ($namedTable -like "*_CL*") {
+                #temporarly store initial custom table name
+                $_customTableName = $CustomTableName
+                $CustomTableName = ($namedTable -replace "_CL.json")
             }
 
             Set-LogAnalyticsData `
-            $paramObject = @{
-                "WorkspaceId" = $workspaceId
-                "WorkspaceKey" = $workspaceKey
-                "body" = ([System.Text.Encoding]::UTF8.GetBytes($content))
-            }
+                -WorkspaceId $workspaceId `
+                -WorkspaceKey $workspaceKey `
+                -body ([System.Text.Encoding]::UTF8.GetBytes($dataFile)) `
+                -logType $CustomTableName
 
-            if ($dataFile.Name -like "*_CL") {
-                $paramObject.logType = $dataFile.Name
+            #reset to initial value
+            if ($_customTableName) {
+                $CustomTableName = $_customTableName
             }
-            else {
-                $paramObject.logType = $CustomTableName
-            }
-
-            Set-LogAnalyticsData @paramObject
         }
+    }
+}
+elseif ($PSCmdlet.ParameterSetName -eq "LocalRepo") {
+    $dataFiles = @(Get-ChildItem `
+        -Path $repoDirectory `
+        -File `
+        -Recurse `
+        -Include "*.json", "*.csv")
+
+    foreach ($dataFile in $dataFiles) {
+        Write-Output "Retrieving content from data file [$dataFile]"
+        switch ($dataFile.extension) {
+            ".json" {
+                Write-Output "Processing [$dataFile]"
+                $content = Get-Content -Path $dataFile.FullName -Raw
+            }
+            ".csv" {
+                Write-Output "Processing [$dataFile]"
+                $content = Get-Content -Path $dataFile.FullName -Raw `
+                | ConvertFrom-Csv `
+                | ConvertTo-Json
+            }
+        }
+
+        Set-LogAnalyticsData `
+        $paramObject = @{
+            "WorkspaceId" = $workspaceId
+            "WorkspaceKey" = $workspaceKey
+            "body" = ([System.Text.Encoding]::UTF8.GetBytes($content))
+        }
+
+        if ($dataFile.Name -like "*_CL") {
+            $paramObject.logType = $dataFile.Name
+        }
+        else {
+            $paramObject.logType = $CustomTableName
+        }
+
+        Set-LogAnalyticsData @paramObject
     }
 }
