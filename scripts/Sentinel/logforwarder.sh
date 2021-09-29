796392b685cef45de09d6c9ad5a0906c1c479317
SecureHats <72928684+SecureHats@users.noreply.github.com>
2021-09-29 16:55:26 +0200

    Update logforwarder.sh

--- scripts/Sentinel/logforwarder.sh
+++ scripts/Sentinel/logforwarder.sh
@@ -50,6 +50,7 @@ fi
 ############################
 # Configure CEF Forwarding #
 ############################
+echo "CONFIGURE CEF FORWARDING"
 sudo alternatives --set python /usr/bin/python3
 sudo setenforce 0
 wget -O onboard_agent.sh https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh --purge
@@ -59,6 +60,7 @@ sleep 15
 ######################
 # Configure Firewall #
 ######################
+echo "CONFIGURE LOCAL FIREWALL"
 sudo firewall-cmd --direct --permanent --add-rule ipv4 filter INPUT 0 -p tcp --dport 25226  -j ACCEPT
 sudo firewall-cmd --permanent --add-port=514/tcp
 sudo firewall-cmd --permanent --add-port=514/udp
@@ -69,13 +71,17 @@ sudo firewall-cmd --direct --get-rules ipv4 filter INPUT
 # Host Name Setting and Keep Time Generated Original #
 ######################################################
 
+echo "CONFIGURE HOSTNAME SETTING"
 sudo sed -i -e "/'Severity' => tags[tags.size - 1]/ a \ \t 'Host' => record['host']" -e "s/'Severity' => tags[tags.size - 1]/&,/" /opt/microsoft/omsagent/plugin/filter_syslog_security.rb && sudo /opt/microsoft/omsagent/bin/service_control restart $WORKSPACE_ID
+
+echo "CONFIGURE LOG TIME SETTINGS"
 sudo wget -O TimeGenerated.py https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/TimeGenerated.py && yes | python TimeGenerated.py $WORKSPACE_ID
 
 ##############################################
 # Re-Enabling and Configuring SELinux Policy #
 #############################################
 
+echo "WRAPPING UP INSTALLATION"
 sudo setenforce 1
 sudo systemctl restart rsyslog.service
 sudo ausearch -c 'rsyslogd' --raw | audit2allow -M my-rsyslogd
