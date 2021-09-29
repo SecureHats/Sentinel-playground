9fb84c35a4347a41768e491fa977e24dc03a1a9a
SecureHats <72928684+SecureHats@users.noreply.github.com>
2021-09-29 16:59:56 +0200

    Update logforwarder.sh

--- scripts/Sentinel/logforwarder.sh
+++ scripts/Sentinel/logforwarder.sh
@@ -53,7 +53,7 @@ fi
 echo "CONFIGURE CEF FORWARDING"
 sudo alternatives --set python /usr/bin/python3
 sudo setenforce 0
-wget -O onboard_agent.sh https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh --purge
+sudo wget -O onboard_agent.sh https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sudo sh onboard_agent.sh --purge
 sudo wget -O cef_installer.py https://raw.githubusercontent.com/Azure/Azure-Sentinel/master/DataConnectors/CEF/cef_installer.py&&sudo python cef_installer.py $WORKSPACE_ID $WORKSPACE_KEY
 sleep 15
 
