d4b6f4d47525c170a946973dc0c5f6f886501034
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2021-11-26 10:12:32 +0100

    updated function

--- PowerShell/Enable-AlertRules/Enable-AlertRules.ps1
+++ PowerShell/Enable-AlertRules/Enable-AlertRules.ps1
@@ -1,85 +1,101 @@
-param(
-    [Parameter(Mandatory=$true)][string]$ResourceGroup,
-    [Parameter(Mandatory=$true)][string]$Workspace
-)
+<#
+.Synopsis
+   Helper function that updates existing detection rules in Microsoft Sentinel
+.DESCRIPTION
+   This helper function updates the existing detection rules in the Microsoft Sentinel portal to match the latest version available in the Alert Templates Catalog
+.EXAMPLE
+   Update-DetectionRules -ResourceGroupName 'MyResourceGroup' -WorkspaceName 'MyWorkspace'
+#>
+function Update-DetectionRules {
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Graph access token
+        [Parameter(Mandatory = $true,
+            Position = 0)]
+        [string]$ResourceGroupName,
 
-$context = Get-AzContext
+        # Graph access token
+        [Parameter(Mandatory = $true,
+            Position = 1)]
+        [string]$WorkspaceName
+    )
 
-if(!$context){
-    Connect-AzAccount
     $context = Get-AzContext
-}
 
-$SubscriptionId = $context.Subscription.Id
+    if (!$context) {
+        Connect-AzAccount
+        $context = Get-AzContext
+    }
 
-Write-Host "Connected to Azure with subscription: " + $context.Subscription
+    $SubscriptionId = $context.Subscription.Id
 
-$baseUri = "/subscriptions/${SubscriptionId}/resourceGroups/${ResourceGroup}/providers/Microsoft.OperationalInsights/workspaces/${Workspace}"
-$templatesUri = "$baseUri/providers/Microsoft.SecurityInsights/alertRuleTemplates?api-version=2019-01-01-preview"
-$alertUri = "$baseUri/providers/Microsoft.SecurityInsights/alertRules/"
+    Write-Host "Connected to Azure with subscription: " + $context.Subscription
 
+    $apiVersion     = "api-version=2021-10-01-preview"
+    $baseUri        = "/subscriptions/${SubscriptionId}/resourceGroups/${ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/${WorkspaceName}"
+    $templatesUri   = "$baseUri/providers/Microsoft.SecurityInsights/alertRuleTemplates?apiVersion"
+    $alertUri       = "$baseUri/providers/Microsoft.SecurityInsights/alertRules/?$apiVersion"
 
-try {
-    $alertRulesTemplates = ((Invoke-AzRestMethod -Path $templatesUri -Method GET).Content | ConvertFrom-Json).value
-}
-catch {
-    Write-Verbose $_
-    Write-Error "Unable to get alert rules with error code: $($_.Exception.Message)" -ErrorAction Stop
-}
+    $alertRulesTemplates = ((Invoke-AzRestMethod -Path "$($templatesUri)" -Method GET).Content | ConvertFrom-Json).value
+    $alerts = ((Invoke-AzRestMethod -Path "$($alertUri)" -Method GET).Content | ConvertFrom-Json).value
 
-$return = @()
-
-$connectors = $alertRulesTemplates.properties.requiredDataConnectors.connectorId | Get-Unique | Sort-Object
-
-if ($Connectors){
+    $i = 0
     foreach ($item in $alertRulesTemplates) {
-        if ($item.kind -eq "Scheduled"){
-            foreach ($connector in $item.properties.requiredDataConnectors) {
-                if ($connector.connectorId -in $Connectors){
-                    $guid = New-Guid
-                    $alertUriGuid = $alertUri + $guid + '?api-version=2021-10-01-preview'
+        if ($item.kind -eq "Scheduled") {
+            foreach ($alert in $alerts) {
+                if ($alert.properties.alertRuleTemplateName -in $item.name -or $alert.properties.displayName -eq $item.properties.displayName) {
+                    $alertUriGuid = $alertUri + $alert.name + '?api-version=2021-10-01-preview'
+                    $i++
+                    Write-Host "Processing $($i) of $($alerts.count): $($item.properties.displayname)" -ForegroundColor Yellow
 
                     $properties = @{
-                    queryFrequency        = $item.properties.queryFrequency
-                    queryPeriod           = $item.properties.queryPeriod
-                    triggerOperator       = $item.properties.triggerOperator
-                    triggerThreshold      = $item.properties.triggerThreshold
-                    severity              = $item.properties.severity
-                    query                 = $item.properties.query
-                    entityMappings        = $item.properties.entityMappings
-                    version               = $item.properties.version
-                    displayName           = $item.properties.displayName
-                    description           = $item.properties.description
-                    enabled               = $true
-                    suppressionDuration   = "PT5H"
-                    suppressionEnabled    = $false
-                    alertRuleTemplateName = $item.name
-                }
+                        queryFrequency        = $item.properties.queryFrequency
+                        queryPeriod           = $item.properties.queryPeriod
+                        triggerOperator       = $item.properties.triggerOperator
+                        triggerThreshold      = $item.properties.triggerThreshold
+                        severity              = $item.properties.severity
+                        query                 = $item.properties.query
+                        entityMappings        = $item.properties.entityMappings
+                        templateversion       = $item.properties.version
+                        displayName           = $item.properties.displayName
+                        description           = $item.properties.description
+                        enabled               = $true
+                        suppressionDuration   = "PT5H"
+                        suppressionEnabled    = $false
+                        alertRuleTemplateName = $item.name
+                    }
 
-                if($item.properties.techniques){
-                    $properties.techniques = $item.properties.techniques
-                }
-                if($item.properties.tactics){
-                    $properties.tactics = $item.properties.tactics
-                }
+                    if ($item.properties.techniques) {
+                        $properties.techniques = $item.properties.techniques
+                    }
+                    if ($item.properties.tactics) {
+                        $properties.tactics = $item.properties.tactics
+                    }
 
                     $alertBody = @{}
                     $alertBody | Add-Member -NotePropertyName kind -NotePropertyValue $item.kind -Force
                     $alertBody | Add-Member -NotePropertyName properties -NotePropertyValue $properties
 
-                    try{
-                        Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 3)
+                    try {
+                        $result = Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 10)
+                        if ($result.statusCode -eq 400) {
+                            # if the existing built-in rule was not created from a template (old versions)
+                            if ((($result.Content | ConvertFrom-Json).error.message) -match 'already exists and was not created by a template') {
+                                Invoke-AzRestMethod -Path $alertUriGuid -Method DELETE
+                                pause 10
+                                Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 10)
+                            }
+                        }
                     }
                     catch {
                         Write-Verbose $_
                         Write-Error "Unable to create alert rule with error code: $($_.Exception.Message)" -ErrorAction Stop
                     }
-
                     break
                 }
             }
         }
     }
 }
-
-return $return
