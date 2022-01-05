ae2f17d42497dffbcbd9b02d4fe1b30f939c5f9b
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2022-01-05 21:55:01 +0100

    Updated item properties
    Added option to set default.
    By default the user settings will be preserved. By setting the 'SetDefault' flag, the rule template defaults will be applied.
    Added extra logging when an error occurs during the post of the rules update.

--- PowerShell/Update-DetectionRules/Update-DetectionRules.ps1
+++ PowerShell/Update-DetectionRules/Update-DetectionRules.ps1
@@ -19,7 +19,11 @@ function Update-DetectionRules {
         # Graph access token
         [Parameter(Mandatory = $true,
             Position = 1)]
-        [string]$WorkspaceName
+        [string]$WorkspaceName,
+
+        [Parameter(Mandatory = $false,
+            Position = 2)]
+        [switch]$SetDefaults
     )
 
     $context = Get-AzContext
@@ -30,8 +34,17 @@ function Update-DetectionRules {
     }
 
     $SubscriptionId = $context.Subscription.Id
+    $logo = "
+     _____                           __  __      __
+    / ___/___  _______  __________  / / / /___ _/ /______
+    \__ \/ _ \/ ___/ / / / ___/ _ \/ /_/ / __ `/ __/ ___/
+   ___/ /  __/ /__/ /_/ / /  /  __/ __  / /_/ / /_(__  )
+  /____/\___/\___/\__,_/_/   \___/_/ /_/\__,_/\__/____/
+    `n"
 
-    Write-Host "Connected to Azure with subscription: " + $context.Subscription
+    Clear-Host
+    Write-Host $logo -ForegroundColor White
+    Write-Output "Connected to Azure with subscriptionId: $($SubscriptionId)`n"
 
     $apiVersion     = "?api-version=2021-10-01-preview"
     $baseUri        = "/subscriptions/${SubscriptionId}/resourceGroups/${ResourceGroupName}/providers/Microsoft.OperationalInsights/workspaces/${WorkspaceName}"
@@ -46,26 +59,36 @@ function Update-DetectionRules {
         if ($item.kind -eq "Scheduled") {
             foreach ($alert in $alerts) {
                 if ($alert.properties.alertRuleTemplateName -in $item.name -or $alert.properties.displayName -eq $item.properties.displayName) {
-                    Write-Host "$($item.properties.displayName)"
+                    Write-Verbose "$($item.properties.displayName)"
                     $alertUriGuid = $alertUri +'/'+ $($alert.name) + $apiVersion
                     $i++
-                    Write-Host "Processing $($i) of $($alerts.count): $($item.properties.displayname)" -ForegroundColor Yellow
+                    Write-Host "Processing $($i) of $($alerts.count): $($item.properties.displayname)" -ForegroundColor Green
 
                     $properties = @{
-                        queryFrequency        = $item.properties.queryFrequency
-                        queryPeriod           = $item.properties.queryPeriod
-                        triggerOperator       = $item.properties.triggerOperator
-                        triggerThreshold      = $item.properties.triggerThreshold
+                        queryFrequency        = $alert.properties.queryFrequency
+                        queryPeriod           = $alert.properties.queryPeriod
+                        triggerOperator       = $alert.properties.triggerOperator
+                        triggerThreshold      = $alert.properties.triggerThreshold
                         severity              = $item.properties.severity
                         query                 = $item.properties.query
                         entityMappings        = $item.properties.entityMappings
-                        templateversion       = $item.properties.version
-                        displayName           = $item.properties.displayName
+                        templateVersion       = $item.properties.version
+                        displayName           = $alert.properties.displayName
                         description           = $item.properties.description
-                        enabled               = $true
-                        suppressionDuration   = "PT5H"
-                        suppressionEnabled    = $false
+                        enabled               = $alert.properties.enabled
+                        suppressionDuration   = $alert.properties.suppressionDuration
+                        suppressionEnabled    = $alert.properties.suppressionEnabled
                         alertRuleTemplateName = $item.name
+                    }
+
+                    if ($SetDefaults) {
+                        $properties.queryFrequency        = $item.properties.queryFrequency
+                        $properties.queryPeriod           = $item.properties.queryPeriod
+                        $properties.triggerOperator       = $item.properties.triggerOperator
+                        $properties.triggerThreshold      = $item.properties.triggerThreshold
+                        $properties.displayName           = $item.properties.displayName
+                        $properties.enabled               = [bool]$item.properties.enabled
+                        $properties.suppressionEnabled    = [bool]$item.properties.suppressionEnabled
                     }
 
                     if ($item.properties.techniques) {
@@ -84,8 +107,11 @@ function Update-DetectionRules {
                         if ($result.statusCode -eq 400) {
                             # if the existing built-in rule was not created from a template (old versions)
                             if ((($result.Content | ConvertFrom-Json).error.message) -match 'already exists and was not created by a template') {
+                                Write-Verbose "Rule was not created from template, recreating rule"
                                 Invoke-AzRestMethod -Path $alertUriGuid -Method DELETE
                                 Invoke-AzRestMethod -Path $alertUriGuid -Method PUT -Payload ($alertBody | ConvertTo-Json -Depth 10)
+                            } else {
+                                Write-Host "Warning: "(($result.Content | ConvertFrom-Json).error.message) -ForegroundColor Red
                             }
                         }
                     }
